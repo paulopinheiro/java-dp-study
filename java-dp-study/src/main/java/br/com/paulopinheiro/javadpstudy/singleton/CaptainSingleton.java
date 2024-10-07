05f78d78b04df18e43de09f5f88750e89817f0b4
Paulo Pinheiro <pinus.sc@gmail.com>
2024-10-07 15:09:51 -0300

    Implement Singleton assignment solution

---
+++ java-dp-study/src/main/java/br/com/paulopinheiro/javadpstudy/singleton/CaptainSingleton.java
@@ -0,0 +1,14 @@
+package br.com.paulopinheiro.javadpstudy.singleton;
+
+public class CaptainSingleton {
+    private CaptainSingleton(){}
+
+    // Bill Pugh method
+    private class InnerClass {
+        private static final CaptainSingleton uniqueInstance = new CaptainSingleton();
+    }
+
+    public static CaptainSingleton getInstance() {
+        return InnerClass.uniqueInstance;
+    }
+}
---
+++ java-dp-study/src/main/java/br/com/paulopinheiro/javadpstudy/singleton/CaptainTester.java
@@ -0,0 +1,16 @@
+package br.com.paulopinheiro.javadpstudy.singleton;
+
+public class CaptainTester {
+    public static void main(String[] args) {
+        System.out.println("Trying to make a captain for our team!");
+        CaptainSingleton c1 = CaptainSingleton.getInstance();
+        if (c1!=null) System.out.println("New Captain selected for our team.");
+        System.out.println();
+        System.out.println("Trying to make another captain for our team!");
+        CaptainSingleton c2 = CaptainSingleton.getInstance();
+        if (c1 == c2) {
+            System.out.println("You already have a Captain for your team. Send him for the toss");
+            System.out.println("c1 and c2 are the same");
+        }
+    }
+}

037fa028820b58110f99188de3a18f703d7f5aa9
Paulo Pinheiro <pinus.sc@gmail.com>
2024-10-23 18:32:57 -0300

    Imported Udemy Course Chain of Responsability source code example

---
+++ java-dp-study/src/main/java/br/com/paulopinheiro/javadpstudy/udemy/chainofresponsability/Currency.java
@@ -0,0 +1,13 @@
+package br.com.paulopinheiro.javadpstudy.udemy.chainofresponsability;
+
+public class Currency {
+    private int amount;
+
+    public Currency(int amt) {
+        amount = amt;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}

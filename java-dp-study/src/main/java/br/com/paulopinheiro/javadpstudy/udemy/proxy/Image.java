73b655bdd6772da3e6391fe43125d7bef38fb7ed
Paulo Pinheiro <pinus.sc@gmail.com>
2024-10-23 16:19:17 -0300

    Imported Udemy Course Proxy source code example

---
+++ java-dp-study/src/main/java/br/com/paulopinheiro/javadpstudy/udemy/proxy/Image.java
@@ -0,0 +1,24 @@
+package br.com.paulopinheiro.javadpstudy.udemy.proxy;
+
+public interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk(this.fileName);
+    }
+
+    @Override
+    public void display()
+    {
+        System.out.println("Displaying " + fileName);
+    }
+
+    private void loadFromDisk(String fileName) {
+        System.out.println("Loading " + fileName);
+    }
+}

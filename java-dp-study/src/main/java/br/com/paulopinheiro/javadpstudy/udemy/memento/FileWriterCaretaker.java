55cb41d791c7956cb9958d41b7d6f8b91096c8a9
Paulo Pinheiro <pinus.sc@gmail.com>
2024-10-31 14:30:02 -0300

    Imported Udemy Course Memento source code example

---
+++ java-dp-study/src/main/java/br/com/paulopinheiro/javadpstudy/udemy/memento/FileWriterCaretaker.java
@@ -0,0 +1,13 @@
+package br.com.paulopinheiro.javadpstudy.udemy.memento;
+
+public class FileWriterCaretaker {
+    private Object obj;
+
+    public void save(FileWriterUtil fileWriter){
+        this.obj=fileWriter.save();
+    }
+
+    public void undo(FileWriterUtil fileWriter){
+        fileWriter.undoToLastSave(obj);
+    }
+}

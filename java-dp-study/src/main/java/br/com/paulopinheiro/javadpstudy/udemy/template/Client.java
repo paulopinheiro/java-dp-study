9ac282fc4fd5b215873d841ea284699693e749f9
Paulo Pinheiro <pinus.sc@gmail.com>
2024-11-12 16:15:47 -0300

    Imported Udemy Course Template source code example

---
+++ java-dp-study/src/main/java/br/com/paulopinheiro/javadpstudy/udemy/template/Client.java
@@ -0,0 +1,15 @@
+package br.com.paulopinheiro.javadpstudy.udemy.template;
+
+public class Client {
+    public static void main(String[] args) {
+        HouseTemplate houseType = new WoodenHouse();
+
+        // using template method
+        houseType.buildHouse();
+
+        System.out.println("************");
+
+        houseType = new GlassHouse();
+        houseType.buildHouse();
+    }
+}
